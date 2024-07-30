b6381e5fcf50cde7221c4f4d9f74f43b080987b1
Comment.Vivre <how_to_live@yeah.net>
2024-07-30 13:09:41 +0800

    feat:修改数据库判断条件

--- src/main/java/JDBC.java
+++ src/main/java/JDBC.java
@@ -10,8 +10,10 @@ public class JDBC {
         Connection connection = DriverManager.getConnection(url,username,password);
         // 4.执行SQL对象Statement，执行SQL的对象
         Statement statement = connection.createStatement();
+
         // 5.执行SQL的对象去执行SQL，返回结果集
         String sql = "SELECT *FROM test.main_table;";
+
         ResultSet resultSet = statement.executeQuery(sql);
         while(resultSet.next()){
             System.out.println("id="+resultSet.getString("id"));
@@ -22,9 +24,16 @@ public class JDBC {
          * executeUpdate()，一般执行DELETE或UPDATE或INSERT语句（这些操作更新数据库，所以是update），返回int，被改变的语句的行数。
          * execute()，不确定是什么类型的SQL语句时可以用这个方法。
          */
-        sql = "insert into test.main_table values(2,'test2')";
-        statement.executeUpdate(sql);
-//        6.释放连接
+        for(int i=4;i<=25;i++){
+            sql = "select * from test.main_table where id="+i;
+            resultSet = statement.executeQuery(sql);
+
+            if(!resultSet.next()){
+                sql = "insert into test.main_table values(" + i +",'test2')";
+                statement.executeUpdate(sql);
+            }
+        }
+        //  6.释放连接
         resultSet.close();
         statement.close();
         connection.close();
